01a2997375a432987395aad534696fc2f74f88ab
thunderbug <thunder@justforfun-gaming.com>
2017-10-05 00:16:16 +0200

    Kopiepasta van de boekenzoekers API

--- inc/config-sample.php
+++ inc/config-sample.php
@@ -1,3 +1,20 @@
-/*
-* config voorbeeld
-*/
+<?php
+/* Config voorbeeld */
+//MySQL Database
+
+//Database host
+$dbHost = "localhost";
+//Database naam
+$dbName = "boekenzoekers";
+//Database username
+$dbUsername = "boekenzoekers";
+//Database password
+$dbPassword = "databasepassword";
+
+//FB Developer
+//https://developers.facebook.com/
+
+//App ID
+$appID = "";
+//App Secret
+$appSecret = "";
