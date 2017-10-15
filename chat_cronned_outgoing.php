1160242e45d2bdaa95c4532e12aa3db7424491dc
Thunder <thunder@justforfun-gaming.com>
2017-10-15 23:18:36 +0200

    Alles wat in comments stond is done

--- chat_cronned_outgoing.php
+++ chat_cronned_outgoing.php
@@ -1,12 +1,4 @@
 <?php
-/*
-* haal alle unique gemeenteid's uit de subscribers tabel (-> A)
-* voor elke gemeenteid in (A):
-* - haal gemeente-naam op uit gemeente tabel (-> A2)
-* - haal alle susbscribers uit de subscribers tabel (-> B)
-* - bevraag de boekenzoekers API (gemeente=A2&output=json&timeFrom=<timestamp laatste check>) (-> C)
-* - voor elk resultaat (C) stuur een bericht naar elke subscriber (B) 
-*/
 
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Cache\DoctrineCache;
--- chat_handle_incoming.php
+++ chat_handle_incoming.php
@@ -1,10 +1,4 @@
 <?php
-/*
-* receives incoming chat requests
-* main funciontality: abonneer op gemeente x en de-abonneer op gemeente y
-* dit wordt weggeschreven in de subscribers tabel met subscriberid (= facebook id) en gemeente id (= gemeente.id)
-*/
-
 
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Cache\DoctrineCache;
